334c49b1fa9f3407d8f078b137ab82d45fb4b582
Juan Diego Suarez <jdsuarez@cromasoft.com>
2020-12-02 18:03:25 -0500

    Se realiza maquetación de columna derecha del RegisterLayout. Se implementa funcionalidad que permite pasar el prop className al svgCircle

--- src/components/layout/RegisterLayout.js
+++ src/components/layout/RegisterLayout.js
@@ -1,9 +1,10 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import React from 'react'
 import SvgCharacter from '../../assets/images/svgFiles/svgRegisterLayout/SvgCharacter';
 import SvgCornerCircle from '../../assets/images/svgFiles/svgRegisterLayout/SvgCornerCircle';
-import SvgLeftCircle from '../../assets/images/svgFiles/svgRegisterLayout/SvgLeftCircle';
+import SvgCircle from '../../assets/images/svgFiles/svgRegisterLayout/SvgCircle';
 import RegisterLayoutStyles from '../../styles/layout/RegisterLayoutStyles';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 const RegisterLayout = ({ children }) => {
   
@@ -13,20 +14,31 @@ const RegisterLayout = ({ children }) => {
     <Grid container spacing={2} className={classes.gridContainer}>
       <Grid container item xs={3} direction="column">
         <Grid className={`${classes.gridItem} ${classes.gridItemCircle}`} item xs={4}>
-          <SvgLeftCircle/>
+          <SvgCircle className={classes.turquoiseCircle}/>
         </Grid>
         <Grid className={`${classes.gridItem} ${classes.gridItemCharacter}`} item xs={6}>
           <SvgCharacter/>
         </Grid>
         <Grid className={`${classes.gridItem} ${classes.gridItemCorner}`} item xs={2}>
-          <SvgCornerCircle/>
+          <SvgCornerCircle className={classes.neonBlueCircle}/>
         </Grid>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={7}>
         Col2
       </Grid>
-      <Grid item xs={3}>
-        Col3
+      <Grid container item xs={2} direction="column">
+        <Grid className={`${classes.gridItem} ${classes.gridItemRightCircle}`} item xs={9}>
+          <SvgCircle className={classes.darkCyanCircle}/>
+        </Grid>
+        <Grid className={`${classes.gridItem} ${classes.gridItemHelp}`} item xs={3}>
+          <Button
+            variant="contained"
+            size="large"
+            className={classes.helpButton}
+            startIcon={<HelpOutlineIcon/>}
+          >
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   )
--- src/styles/layout/RegisterLayoutStyles.js
+++ src/styles/layout/RegisterLayoutStyles.js
@@ -1,13 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const RegisterLayoutStyles = makeStyles((theme) => ({
-
   gridContainer: {
     width: "100%",
     height: "100vh",
     margin: "0",
     padding: "0",
-    backgroundColor: "#ECECEC",
+    backgroundColor: theme.palette.colors.background,
     '& .MuiGrid-container': {
       padding: '0'
     }
@@ -24,14 +23,64 @@ const RegisterLayoutStyles = makeStyles((theme) => ({
     paddingTop: '20%'
   },
 
+  gridItemRightCircle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: '25%'
+  },
+
   gridItemCharacter: {
     justifyContent: 'center',
-    paddingLeft: '10%',
+    paddingLeft: '8%',
     paddingTop: '20%'
+  },
+
+  gridItemHelp: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingRight: '10%',
+    paddingBottom: '10%'
   },
 
   gridItemCorner: {
     alignItems: 'flex-end'
+  },
+
+  turquoiseCircle: {
+    '& path': {
+      fill: theme.palette.colors.turquoise
+    }
+  },
+  
+  neonBlueCircle: {
+    '& path': {
+      fill: theme.palette.colors.neonBlue
+    }
+  },
+
+  darkCyanCircle: {
+    width: '124px',
+    height: '121px',
+    '& path': {
+      fill: theme.palette.colors.darkCyan
+    }
+  },
+
+  helpButton: {
+    width: '86px',
+    height: '85px',
+    borderRadius: '50%',
+    fontSize: '20px',
+    backgroundColor: theme.palette.colors.orange,
+    '&:hover': {
+      backgroundColor: theme.palette.colors.red
+    },
+    '& .MuiButton-startIcon': {
+      marginRight: '0'
+    },
+    '& .MuiSvgIcon-root': {
+      fontSize: '38px'
+    }
   }
 
 }));
